f334419fad2f0df14185f2d0eb6a2764f0b2a36b
stpolina <58559351+stpolina@users.noreply.github.com>
2021-04-10 15:48:51 +0300

    Add files via upload

--- buyTicket.py
+++ buyTicket.py
@@ -3,7 +3,7 @@ import json
 import requests
 
 # Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
-url = os.environ.get('CLOUDAMQP_URL', 'amqps://avfepwdu:SS4fTAg36RK1hPQAUnyC6TH-4Mf3uyJo@fox.rmq.cloudamqp.com/avfepwdu')
+url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@206.189.60.128')
 params = pika.URLParameters(url)
 connection = pika.BlockingConnection(params)
 channel = connection.channel() # start a channel
@@ -11,26 +11,27 @@ channel.queue_declare(queue='CashboxBuyTicket', durable = True)
 def callback(ch, method, properties, body):
   body_json = json.loads(body.decode("utf-8"))
   isAvailable = False
+  passengerId = body_json.get('PassengerId')
   print(body_json)
   if body_json.get('Ticket')==None:
       flightId = body_json.get('FlightId')
       #здесь будет get запрос для получение доступных рейсов
-      #http_response = requests.get('')
-      http_response = b'[{"FlightId":"1","HasVips":true}, {"FlightId":"2","HasVips":true}, {"FlightId":"3","HasVips":true}]' #просто для теста
-      response_json = json.loads(http_response.decode("utf-8"))
+      http_response = requests.get('https://info-panel222.herokuapp.com/api/v1/info-panel/all')
+      #http_response = b'[{"FlightId":"1","HasVips":true}, {"FlightId":"2","HasVips":true}, {"FlightId":"3","HasVips":true}]' #просто для теста
+      response_json = json.loads(http_response.text)
       for i in range(len(response_json)):
-          if (response_json[i].get('FlightId') == flightId):
+          if (response_json[i].get('id') == flightId):
               isAvailable = True
       if (isAvailable):
           body_json.update({'Ticket': True})
-          print("Passenger", body_json.get('PassengerId'), "bought ticket")
+          print("Passenger", passengerId, "bought ticket")
       else:
           body_json.update({'IsBought': False})
-          print("Passenger", body_json.get('PassengerId'), "tried to buy a ticket for unavailable flight")
+          print("Passenger", passengerId, "tried to buy a ticket for unavailable flight")
   else:
       #добавить проверку на то, что пассажир не покупает билет на рейс, на который он уже купил билет
       body_json.update({'IsBought': False})
-      print("Passenger", body_json.get('PassengerId'), "can't buy ticket")
+      print("Passenger", passengerId, "can't buy ticket")
   channel.basic_publish(exchange='',
                         routing_key='BuyPassengerQueue',
                         body=json.dumps(body_json))
--- refundTicket.py
+++ refundTicket.py
@@ -2,21 +2,22 @@ import pika, os
 import json
 
 # Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
-url = os.environ.get('CLOUDAMQP_URL', 'amqps://avfepwdu:SS4fTAg36RK1hPQAUnyC6TH-4Mf3uyJo@fox.rmq.cloudamqp.com/avfepwdu')
+url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@206.189.60.128')
 params = pika.URLParameters(url)
 connection = pika.BlockingConnection(params)
 channel = connection.channel() # start a channel
 channel.queue_declare(queue='CashboxRefundTicket', durable = True)
 def callback(ch, method, properties, body):
   body_json = json.loads(body.decode("utf-8"))
+  passengerId = body_json.get('PassengerId')
   print(body_json)
   if body_json.get('Ticket')==None:
       body_json.update({'IsRefunded': False})
-      print("Passenger", body_json.get('PassengerId'), "has no ticket to refund")
+      print("Passenger", passengerId, "has no ticket to refund")
   else:
       body_json.update({'Ticket': None})
       body_json.update({'IsRefunded': True})
-      print("Passenger", body_json.get('PassengerId'), "refunded ticket")
+      print("Passenger", passengerId, "refunded ticket")
   channel.basic_publish(exchange='',
                         routing_key='RefundPassengerQueue',
                         body=json.dumps(body_json))
